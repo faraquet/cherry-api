04696cb0138c01b0166bd644628e9d16d8147fb2
Andrey Andriychuk <andrey@halalbooking.com>
2022-01-28 16:47:57 +0300

    Add generation

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,5 +1,16 @@
 class PdfController < ApplicationController
-  def index
-    render(json: { a: 1 })
+  def create
+    browser = Ferrum::Browser.new
+    filename = "temp-#{DateTime.now.strftime('%Q')}"
+    File.open("tmp/#{filename}.html", "w") { |file| file.write(params[:content]) }
+
+    browser.go_to("file:///#{Rails.root}/tmp/#{filename}.html")
+    pdf_options = { path: "tmp/#{filename}.pdf" }
+
+    pdf_options[:landscape] = ActiveRecord::Type::Boolean.new.deserialize(params[:landscape])
+
+    browser.pdf(**pdf_options)
+
+    send_file "tmp/#{filename}.pdf", type: "pdf"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post "register" => "users#register"
   post "login" => "authentication#login"
-  post "html_to_pdf" => "pdf#index"
+  post "html_to_pdf" => "pdf#create"
 end
