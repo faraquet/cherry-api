0b7caeecd8d264d2abda0513fdc679df01864698
Georgiy Bykov <zhora.bykov@gmail.com>
2022-02-02 23:41:39 +0300

    Fix app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fernatra_production"
+  # config.active_job.queue_name_prefix = "cherry_pdf_production"
 
   config.action_mailer.perform_caching = false
 
