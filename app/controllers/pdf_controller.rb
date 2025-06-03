7023b4681db27c83bbdf7d89509c76ea04e002a2
Andriichuk <andreiandriichuk@gmail.com>
2025-06-03 12:23:01 +0100

    Make it in-memory

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -6,24 +6,50 @@ class PdfController < ApplicationController
   def create
     params.permit!
 
-    browser = Ferrum::Browser.new(timeout: 300, browser_options: { 'no-sandbox': nil })
-    filename = "temp-pdf-#{DateTime.now.strftime('%Q')}"
-    File.write("tmp/#{filename}.html", params[:content])
+    # Create a data URL instead of writing to file
+    html_content = params[:content]
+    encoded_html = Base64.strict_encode64(html_content)
+    data_url = "data:text/html;base64,#{encoded_html}"
 
-    browser.go_to("file:///#{Rails.root.join("tmp/#{filename}.html")}")
-    pdf_options = { path: "tmp/#{filename}.pdf" }
+    browser = Ferrum::Browser.new(
+      timeout: 30,
+      headless: true,
+      browser_options: {
+        'no-sandbox': nil,
+        'disable-web-security': nil,
+        'allow-file-access-from-files': nil
+      }
+    )
+
+    begin
+      browser.go_to(data_url)
+    rescue => e
+      Rails.logger.error "Browser navigation failed: #{e.message}"
+      raise e
+    end
+
+    # Generate PDF in memory (no path specified)
+    pdf_options = {}
 
     if params[:landscape].present?
       pdf_options[:landscape] =
         ActiveRecord::Type::Boolean.new.deserialize(params[:landscape])
     end
 
-    if params[:scale].present? # rubocop:disable Style/IfUnlessModifier
+    if params[:scale].present?
       pdf_options[:scale] = ActiveRecord::Type::Float.new.deserialize(params[:scale])
     end
 
-    browser.pdf(**pdf_options)
+    # Get PDF as binary data
+    pdf_data = browser.pdf(**pdf_options)
 
-    send_file "tmp/#{filename}.pdf", type: 'pdf'
+    # Send the PDF data directly without writing to file
+    send_data pdf_data,
+      type: 'application/pdf',
+      filename: "generated-#{DateTime.now.strftime('%Y%m%d_%H%M%S')}.pdf",
+      disposition: 'attachment'
+
+  ensure
+    browser&.quit
   end
 end
